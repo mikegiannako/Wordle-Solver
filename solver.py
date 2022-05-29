ed20eacd7996d16a682cdd538887cc166e2f130a
Mike G <csd4612@csd.uoc.gr>
2022-05-29 23:19:10 +0300

    adjusting regex correctly

--- solver.py
+++ solver.py
@@ -1,3 +1,4 @@
+from warnings import filterwarnings
 from selenium.webdriver.chrome.options import Options as Chrome_Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.remote.webelement import WebElement
@@ -8,6 +9,47 @@ import re
 # Gets the words from the website specified in word_scrapper.py
 from word_scrapper import get_words
 words = get_words()
+
+letters = 'abcdefghijklmnopqrstuvwxyz'
+
+# List of a set of characters that can't be used for every letter of the word
+filters = [set()] * 5
+present = set()
+
+def regulate_filters(result):
+    global filters
+    global present
+
+    # We need this so in case where a single letter appears more than once in
+    # a word it's correct in a certain position but not in others, we remove
+    # the option only for particular cells
+    correct = set()
+
+    # First patching all the letters that are correct to make next checks easier
+    for i,res in enumerate(result):
+        # As there's only one correct letter, we remove all else
+        if res[1] == 'correct': 
+            filters[i] = set(letters) - {res[0]}
+            correct.add(res[0])
+            if res[0] in present: present.remove(res[0])
+
+    # Afterwards we check for all the other cases
+    for i,res in enumerate(result):
+        if res[1] == 'absent':
+            if res[0] in correct: filters[i].add(res[0])
+            else: 
+                for j in range(len(filters)): filters[j].add(res[0])
+        elif res[1] == 'present':
+            present.add(res[0])
+
+def generate_regex():
+    global filters
+
+    regex = ''
+    for filter in filters:
+        regex += f'[^{"".join(filter)}]' if len(filter) > 0 else '\.'
+    
+    return regex
 
 def find_result(browser, iteration):
     # Find the state of the letters in the #iteration line and returns them in
@@ -26,12 +68,12 @@ def find_result(browser, iteration):
     
     return re.findall(r'letter="([a-z])" evaluation="(absent|correct|present)"',result.get_property('innerHTML'))
 
-def try_word(browser):
+def try_word(browser, word):
     # Finding the text area (which is virtually anywhere)
     text_area = browser.find_element_by_css_selector('body')
     
     # Typing the word
-    text_area.send_keys('kappa')
+    text_area.send_keys(word)
     # Submit the word
     text_area.send_keys(Keys.ENTER)
 
@@ -57,18 +99,24 @@ def main():
     # Clicking anywhere for the tutorial to disappear
     browser.find_element_by_css_selector('body').click()
 
-
+    # Tries maximum 6 words, break the loop if the word is correct
     for i in range(6):
-        # Types and submits the word
-        try_word(browser)
+        # Types and submits the word, the first word will always be 'steam'
+        try_word(browser, "kappa"if i != 0 else 'steam')
 
         # Necessary to wait for the page to load because of animations
+        # implicitly_wait didn't do the job
         sleep(3)
 
+        # Getting the result of the word typed
         result = find_result(browser, i + 1)
+
         if all([res[1] == 'correct' for res in result]):
             print(f'Iteration {i + 1} is correct')
             break
+
+        regulate_filters(result)
+        print(generate_regex())
 
 
 if __name__ == '__main__':
