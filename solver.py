6dab4e4f84dbb70a5f0c904ad5c75a3dc25bff14
Mike G <csd4612@csd.uoc.gr>
2022-05-29 22:16:23 +0300

    Getting result and checking if puzzle is solved

--- solver.py
+++ solver.py
@@ -1,24 +1,75 @@
-# Enter https://www.nytimes.com/games/wordle/ using selenium
 from selenium.webdriver.chrome.options import Options as Chrome_Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.remote.webelement import WebElement
 from selenium import webdriver
 from time import sleep
+import re
 
-chrome_options = Chrome_Options()
+# Gets the words from the website specified in word_scrapper.py
+from word_scrapper import get_words
+words = get_words()
 
-with open("driver_path", "r") as f:
-    driver_path = f.readlines()[0]
+def find_result(browser, iteration):
+    # Find the state of the letters in the #iteration line and returns them in
+    # a list like such: [('s', 'absent'), ('t', 'absent'), ('e', 'absent'), ('a', 'correct'), ('m', 'absent')]
 
-browser = webdriver.Chrome(executable_path=driver_path, options=chrome_options)
-browser.implicitly_wait(5)
+    result: WebElement = browser.execute_script(f"""
+                                    return document
+                                    .querySelector('game-app')
+                                    .shadowRoot
+                                    .querySelector('#game')
+                                    .querySelector('#board-container')
+                                    .querySelector('game-row:nth-child({iteration})')
+                                    .shadowRoot
+                                    .querySelector('div.row')
+                                    """)
+    
+    return re.findall(r'letter="([a-z])" evaluation="(absent|correct|present)"',result.get_property('innerHTML'))
 
-browser.get('https://www.nytimes.com/games/wordle/')
-browser.maximize_window()
+def try_word(browser):
+    # Finding the text area (which is virtually anywhere)
+    text_area = browser.find_element_by_css_selector('body')
+    
+    # Typing the word
+    text_area.send_keys('kappa')
+    # Submit the word
+    text_area.send_keys(Keys.ENTER)
 
-browser.implicitly_wait(2)
+def main():
+    # You should add a "driver_path" file with the path to your chromedriver.exe
+    with open("driver_path", "r") as f:
+        driver_path = f.readlines()[0].strip()
 
-reject_button = browser.find_element_by_xpath('//*[@id="pz-gdpr-btn-reject"]')
-reject_button.click()
+    # Initializing the browser
+    browser = webdriver.Chrome(executable_path=driver_path, options=Chrome_Options())
+    # Entering the website
+    browser.get('https://www.nytimes.com/games/wordle/')
+    # Maximizing the browser window
+    browser.maximize_window()
+    # Waiting for the page to load
+    browser.implicitly_wait(2)
 
-browser.find_element_by_css_selector('body').click()
+    # Finding the reject cookies button
+    reject_button = browser.find_element_by_xpath('//*[@id="pz-gdpr-btn-reject"]')
+    # and clicking it
+    reject_button.click()
 
-sleep(60)
+    # Clicking anywhere for the tutorial to disappear
+    browser.find_element_by_css_selector('body').click()
+
+
+    for i in range(6):
+        # Types and submits the word
+        try_word(browser)
+
+        # Necessary to wait for the page to load because of animations
+        sleep(3)
+
+        result = find_result(browser, i + 1)
+        if all([res[1] == 'correct' for res in result]):
+            print(f'Iteration {i + 1} is correct')
+            break
+
+
+if __name__ == '__main__':
+    main()
