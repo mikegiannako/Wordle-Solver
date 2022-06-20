da1f2e16b0f431d09d2c3a0c9555acb174611075
Mike G <csd4612@csd.uoc.gr>
2022-06-20 21:21:25 +0300

    The previous source stopped working

--- word_scrapper.py
+++ word_scrapper.py
@@ -5,7 +5,7 @@ def get_words():
     from bs4 import BeautifulSoup
     from urllib.request import urlopen
 
-    url = 'https://www-cs-faculty.stanford.edu/~knuth/sgb-words.txt'
+    url = 'https://gist.githubusercontent.com/dracos/dd0668f281e685bad51479e5acaadb93/raw/ca9018b32e963292473841fb55fd5a62176769b5/valid-wordle-words.txt'
     html = urlopen(url)
     soup = BeautifulSoup(html, 'html.parser')
 
